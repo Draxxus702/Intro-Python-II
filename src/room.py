cb44c8dd6ff69b394642b56181de316eefe7fffa
Draxxus702 <58435416+KennithHowe2@users.noreply.github.com>
2020-06-04 13:35:01 -0700

    added character and room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, descriptiopn. n_to=None, e_to=None, s_to=None, w_to=None)
+    self.name=name
+    self.descrition=description
+    self.connection={
+        "n": n_to
+        "e": e_to
+        "s": s_to
+        "w": w_to
+    }
+    self.n_to=n_to
+    self.e_to=e_to
+    self.s_to=s_to
+    self.w_to=w_to
